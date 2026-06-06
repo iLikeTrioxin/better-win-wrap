f052c2d85d41eb0ebd9b3433a47818ce7ed834af
iLikeTrioxin <dziedzickamil91@gmail.com>
2026-06-07 01:01:16 +0200

    .

--- main.cpp
+++ main.cpp
@@ -157,8 +157,8 @@ int addWidget(lua_State* L) {
     const auto PMONITOR = widget.window->m_monitor.lock();
     const auto& box = widget.window->layoutTarget()->position();
     if(widget.position.x < 0 || widget.position.y < 0){
-        widget.position.x = box.x / PMONITOR->m_position.x;
-        widget.position.y = box.y / PMONITOR->m_position.y;
+        widget.position.x = box.x / PMONITOR->m_size.x;
+        widget.position.y = box.y / PMONITOR->m_size.y;
     }
 
     if(widget.size.x < 0 || widget.size.y < 0){
@@ -166,8 +166,7 @@ int addWidget(lua_State* L) {
         widget.size.y = box.h / PMONITOR->m_size.y;
     }
 
-    HyprlandAPI::addNotification(PHANDLE, "[hw] x: " + std::to_string(PMONITOR->m_position.x) + ", y: " + std::to_string(PMONITOR->m_position.y) + ", w: " + std::to_string(PMONITOR->m_size.x) + ", h" + std::to_string(PMONITOR->m_size.y) + "ok", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
-    HyprlandAPI::addNotification(PHANDLE, "[hw] x: " + std::to_string(box.x) + ", y: " + std::to_string(box.y) + ", w: " + std::to_string(box.w) + ", h" + std::to_string(box.h) + "ok", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+    HyprlandAPI::addNotification(PHANDLE, "[hw] x: " + std::to_string(widget.position.x) + ", y: " + std::to_string(widget.position.y) + ", w: " + std::to_string(widget.size.x) + ", h" + std::to_string(widget.size.y) + "ok", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
     widget.window->m_ruleApplicator->m_tagKeeper.applyTag("+" + widget.tag, true);
     widget.window->m_ruleApplicator->propertiesChanged(Desktop::Rule::RULE_PROP_TAG);
     widget.window->updateDecorationValues();
